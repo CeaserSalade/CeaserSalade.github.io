7ad9aede0e600f74d17fb359a29f3900ef69cd58
Ceaser Salad <72069767+CeaserSalade@users.noreply.github.com>
2024-06-17 21:17:56 +0200

    parallax
    still some kinks to be fixed but it is here

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// Menu for mobile
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
@@ -16,3 +18,22 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
 }
+
+// Parallax
+
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll', () =>{
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill1.style.top = value * 1.1 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+});
